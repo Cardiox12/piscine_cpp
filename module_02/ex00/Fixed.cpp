c18247ea214b5afa4a970b6880f8d79895d2fecd
Tony <com.bellavi@tutanota.com>
2021-01-06 09:17:06 +0100

    Finish module02/ex00

--- module_02/ex00/Fixed.cpp
+++ module_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 16:38:22 by bbellavi          #+#    #+#             */
-/*   Updated: 2021/01/05 16:55:14 by bbellavi         ###   ########.fr       */
+/*   Updated: 2021/01/06 09:15:47 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,45 @@
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor called" << std::endl;
+}
 
+Fixed::Fixed()
+{
+	_fixed = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(int fixed)
+{
+	_fixed = fixed;
+}
+
+Fixed::Fixed(const Fixed &cls)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_fixed = cls.getRawBits();
 }
 
 Fixed
-&Fixed::operator=(const Fixed &fixed)
+&Fixed::operator=(const Fixed &cls)
 {
-	_fixed = fixed._fixed;
-	
+	_fixed = cls.getRawBits();
+
+	std::cout << "Assignation operator called" << std::endl;
+
 	return (*this);
 }
 
 int
-Fixed::getFixed() const
+Fixed::getRawBits(void) const
 {
+	std::cout << "getRawBits member function called" << std::endl;
 	return (_fixed);
+}
+
+void
+Fixed::setRawBits(int const raw)
+{
+	_fixed = raw;
 }
--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -6,27 +6,30 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 16:34:56 by bbellavi          #+#    #+#             */
-/*   Updated: 2021/01/05 16:55:08 by bbellavi         ###   ########.fr       */
+/*   Updated: 2021/01/06 09:15:52 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
-#define FIXED_HPP
+# define FIXED_HPP
 
-class Fixed
+# include <iostream>
+
+class Fixed 
 {
 private:
 	static const int	_MAX_BITS = 8;
 	int					_fixed;
 
 public:
-	Fixed() : _fixed{ 0 } {};
-	Fixed(int fixed) : _fixed{ fixed } {};
-	Fixed(const Fixed &cls) : _fixed{ cls._fixed } {};
+	Fixed();
+	Fixed(int fixed);
+	Fixed(const Fixed &cls);
 	~Fixed();
-	Fixed	&operator=(const Fixed &fixed);
+	Fixed	&operator=(const Fixed &cls);
 
-	int	getFixed() const;
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
 };
 
 #endif
